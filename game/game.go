63556d7e872960d9e9c59168e09a3b1027d95a42
Daniel Hiller <dhiller@redhat.com>
2021-03-03 10:57:48 +0100

    after dl code, add solution

--- game/game.go
+++ game/game.go
@@ -2,32 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"unsafe"
+)
+
+const (
+	MaxX = 1000
+	MaxY = 1000
 )
 
 func main() {
 	var p Player
 	fmt.Println("player size:", unsafe.Sizeof(p))
 
-	p, err := NewPlayer("Parzival", 10, 20)
-	fmt.Println("p = ", p)
-	fmt.Println("err = ", err)
+	/* Before Object
+	p = Player{"Parzival", 10, 20}
+	fmt.Println("p =", p)
 	fmt.Printf("%%+v: %+v\n", p)
 	fmt.Printf("%%#v: %#v\n", p) // used in logging
-	fmt.Println("player size:", unsafe.Sizeof(p))
+	*/
 
-	p.Move(200,300)
-	fmt.Println("p = ", p)
+	p1, err := NewPlayer("Parzival", 100, 200)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("p1", p1)
 
-	c := Car{10, Object{17, 42}}
-	fmt.Println("c = ", c)
-	c.Object.Y = 37
-	c.X = 27
-	fmt.Println("c = ", c)
+	p1.Move(700, 873)
+	fmt.Println("p1 (move)", p1)
 
-	player := Player{"test2", Object{99, 88}}
-	car := Car{22, Object{77, 66}}
-	fmt.Println("GatherAll = ", GatherAll([]mover{&p, &Player{}, &player, &car}, 22, 33))
+	if err := GatherAll([]mover{&p1, &Player{}, &Car{}}, 35, 3); err != nil {
+		log.Fatalf("can't move: %s", err)
+	}
+
+	/*
+		var m1 mover = &p1
+		fmt.Println(m1)
+	*/
+
+	p1.FoundKey(Jade)
+	fmt.Println("keys:", p1.Keys)
+	p1.FoundKey(Crystal)
+	fmt.Println("keys:", p1.Keys)
 }
 
 type mover interface {
@@ -35,12 +51,13 @@ type mover interface {
 }
 
 func GatherAll(pieces []mover, x, y int) error {
-	// FIXME: transaction
+	// FIXME: transaction (see STM)
 	for _, p := range pieces {
 		if err := p.Move(x, y); err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
@@ -49,23 +66,96 @@ type Object struct {
 	Y int
 }
 
-type Car struct {
-	Length int
-	// X int // you could shadow the X in the struct
-	Object // embedding
-}
-
-func (p *Object) Move(x,y int) error {
-	p.X = x
-	p.Y = y
+func (o *Object) Move(x, y int) error {
+	// FIXME: Bounds check
+	o.X = x
+	o.Y = y
 	return nil
 }
 
+type Car struct {
+	Object
+	Length int
+}
+
 func NewPlayer(name string, x, y int) (Player, error) {
-	return Player{name, Object{x, y}}, nil
+	if name == "" {
+		return Player{}, fmt.Errorf("empty name")
+	}
+	if x < 0 || x > MaxX || y < 0 || y > MaxY {
+		return Player{}, fmt.Errorf("(%d, %d) out of bounds (%d, %d)", x, y, MaxX, MaxY)
+	}
+
+	/*
+		var p Player
+		p.Name = name
+		p.X = x
+		p.Y = y
+	*/
+
+	p := Player{
+		Name:   name,
+		Object: Object{x, y},
+	}
+	return p, nil
+}
+
+type Key byte // byte is an alias to uint8 (rune = int32, a Unicode code-point/character)
+
+const (
+	Jade    Key = 1 << iota // 1<<0 = 1 (0001)
+	Copper                  // 1<<1 = 2 (0010)
+	Crystal                 // 1<<2 = 4 (0100)
+)
+
+// implement the fmt.Stringer interface
+func (k Key) String() (result string) {
+	keys := []Key{Jade, Copper, Crystal}
+	for _, key := range keys {
+		r := k & key
+		if r > 0 {
+			if len(result) > 0 {
+				result += "|"
+			}
+			result += getKeyString(r)
+		}
+	}
+	return result
+}
+
+func getKeyString(k Key) string {
+	switch k {
+	case 0:
+		return "<nil>"
+	case Jade:
+		return "Jade"
+	case Copper:
+		return "Copper"
+	case Crystal:
+		return "Crystal"
+	}
+
+	// Careful: don't use %s or %v verbs here
+	return fmt.Sprintf("unknown key: %d", k)
+}
+
+func (p *Player) FoundKey(k Key) {
+	p.Keys |= k
 }
 
 type Player struct {
 	Name string
-	Object // embedding
+	//	X      float64 // will "shadow" the Object.X
+	Object // struct embedding
+	Keys   Key
 }
+
+/*
+type Reader interface {
+    Read(p []byte) (n int, err error)
+}
+
+type Reader interface {
+    Read(n int) (p []byte, err error)
+}
+*/
